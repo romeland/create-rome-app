70dc35a479ec49acdfdc51bb55e99633f85bd9dd
FrankQiu <iam.frankqiu@gmail.com>
2021-05-28 11:37:07 +0800

    feat: add node version check

---
+++ packages/create-rome-app/index.ts
@@ -0,0 +1,13 @@
+#!/usr/bin/env ts-node
+import semver from 'semver'
+
+
+const currentVersion = process.versions.node
+const minimalRequiredVersion = '13.2.0'
+
+if (semver.lt(currentVersion, minimalRequiredVersion)) {
+    console.error(`The version of Node.js is too low. Please update to ${minimalRequiredVersion} or higher`)
+    process.exit(1)
+}
+
+
